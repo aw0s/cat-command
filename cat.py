dfdc46791b1436df14f8dec19609f534e80fcf94
aw0s <michal.sowa200000@gmail.com>
2021-01-23 18:35:07 +0100

    Add basic -b and -n options

--- cat.py
+++ cat.py
@@ -1,35 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from sys import exit
 import os
 from argparse import ArgumentParser
 
 
-def main():
+def create_parser():
     parser = ArgumentParser()
+
     parser.add_argument(nargs='*', dest='files')
+    parser.add_argument('-n', '--number', action='store_true')
+    parser.add_argument('-b', '--number_notblank', action='store_true')
+    parser.add_argument('-E', action='store_true')
+
     args = parser.parse_args()
+
+    return args
+
+
+def read_print(path: str, options):
+    """Prints file content - line by line."""
+    with open(path, 'r') as f:
+        readl = f.readlines()
+
+    if options.number_notblank:
+        on_minus = 0
+        for index, line in enumerate(readl, start=1):
+            line = line.replace('\n', '')
+            if line:
+                print(f"     {index - on_minus}  {line}")
+            else:
+                print()
+                on_minus += 1
+
+    elif options.number:
+        for index, line in enumerate(readl, start=1):
+            line = line.replace('\n', '')
+            print(f"     {index}  {line}")
+
+
+
+def main():
+    args = create_parser()
 
     cwd = os.getcwd()
 
-    for file in args.files:
-        if os.path.exists(file) and os.path.exists(f'{cwd}/{file}'):
-            try:
-                with open(f'{file}', 'r') as f:
-                    readl = f.readlines()
-
-                for line in readl:
-                    print(line.replace('\n', ''))
-            except FileNotFoundError:
-                with open(f'{cwd}/{file}', 'r') as f:
-                    readl = f.readlines()
-
-                for line in readl:
-                    print(line)
-        else:
-            print(f"{file}: No such file or directory.")
-            exit(0)
+    if args.files:
+        for file_path in args.files:
+            if os.path.exists(file_path) and os.path.exists(f'{cwd}/{file_path}'):
+                try:
+                    read_print(path=file_path, options=args)
+                except FileNotFoundError:
+                    read_print(path=f'{cwd}/{file_path}', options=args)
+            else:
+                print(f"{file_path}: No such file or directory.")
+    else:
+        while True:
+            inp = input()
+            print(inp)
 
 
 if __name__ == '__main__':
